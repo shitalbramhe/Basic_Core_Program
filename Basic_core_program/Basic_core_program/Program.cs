ca6517337d3548a72317667d28e90ed2f7c292c8
shital bramhe <shitalbramhe@gmail.com>
2021-11-10 19:56:03 +0530

    [shital] Add : initial

---
+++ Basic_core_program/Basic_core_program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Basic_core_program
+{
+    public class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to basic core proogram");
+            Console.WriteLine("Enter number  \n 1. Flip_Coin \n 2. Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    Flip_Coin Flip = new Flip_Coin();
+                    Flip.Checkpercentage();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    break;
+            }
+        }
+    }
+}
