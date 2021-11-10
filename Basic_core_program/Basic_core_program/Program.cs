093541550e2a894ee568e97f008a9983cb8613e9
shital bramhe <shitalbramhe@gmail.com>
2021-11-10 20:30:06 +0530

    [shital] Add : Enter year is leap year or not

--- Basic_core_program/Basic_core_program/Program.cs
+++ Basic_core_program/Basic_core_program/Program.cs
@@ -7,13 +7,17 @@ namespace Basic_core_program
          static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic core proogram");
-            Console.WriteLine("Enter number  \n 1. Flip_Coin \n 2. Exit");
+            Console.WriteLine("Enter number  \n 1. Flip_Coin \n 2. Leap_year \n 3. Exit");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch(choice)
             {
                 case 1:
                     Flip_Coin Flip = new Flip_Coin();
                     Flip.Checkpercentage();
+                    break;
+                case 2:
+                    Leap_year Leap = new Leap_year();
+                    Leap.Check_leap_year();
                     break;
 
                 default:
