e4d7cdf86b5daf44852bb0aaab939a17f23847fb
shital bramhe <shitalbramhe@gmail.com>
2021-11-13 23:05:29 +0530

    [shital] ADD: largest among three number

---
+++ Basic_core_program/Basic_core_program/Largest_among_num.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_core_program
+{
+    internal class Largest_among_num
+    {
+        public void largest_num(int num1, int num2, int num3)
+        {
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one is the largest!\n");
+                }
+                else
+                {
+                    Console.Write("Number three is the largest!\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("Number two is the largest!\n");
+            else
+                Console.Write("Number three is the largest!\n");
+        }
+    }
+}
--- Basic_core_program/Basic_core_program/Program.cs
+++ Basic_core_program/Basic_core_program/Program.cs
@@ -7,7 +7,7 @@ namespace Basic_core_program
          static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic core proogram");
-            Console.WriteLine("Enter number  \n 1. Flip_Coin \n 2. Leap_year \n 3. Power of Two \n 4. Harmonic Number \n 5. Prime Factor \n 6. Compute Quotient and Remainder \n 7. Swap two numbers \n 8. Even odd  \n 9. Check Vowel consonant");
+            Console.WriteLine("Enter number  \n 1. Flip_Coin \n 2. Leap_year \n 3. Power of Two \n 4. Harmonic Number \n 5. Prime Factor \n 6. Compute Quotient and Remainder \n 7. Swap two numbers \n 8. Even odd  \n 9. Check Vowel consonant \n 10.  Largest among three number");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch(choice)
             {
@@ -59,6 +59,16 @@ namespace Basic_core_program
                     Console.WriteLine("Enter charactor");
                     char alphabet = Convert.ToChar(Console.ReadLine());
                     vowelconsonant.vowel_consonant(alphabet);
+                    break;
+                case 10:
+                    Largest_among_num large = new Largest_among_num();
+                    Console.WriteLine("Enter first number");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter second number");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter third number");
+                    int number3 = Convert.ToInt32(Console.ReadLine());
+                    large.largest_num(number1, number2, number3);
                     break;
                 default:
                     Console.WriteLine("Invalid Selection");
